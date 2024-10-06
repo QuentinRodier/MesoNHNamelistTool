2214291f8abe64acdbde4f89d3bf3057990b7415
Quentin Rodier <quentin.rodier@meteo.fr>
2024-10-06 22:31:51 +0200

    Quentin 06/10/2024: add checks of duplicates of sub-groups

--- check.py
+++ check.py
@@ -1,14 +1,38 @@
+from iteration_utilities import duplicates
+
 def checkFormat(nam):
         """
        Control the format of the namelist:
        - missing / at the end of a sub-group
-       - duplicates of &NAM_XXX
+       - (OK) duplicates of &NAM_XXX
        - duplicates of keys
        - missing & in front of a sub-group
        - extra . after a real number (e.g. '2.0.')',
        - real number sent to an integer variable (starting by N or I) 
         """
-        
+        # Look for all sub-groups
+        subGroups = []
+        inSub = False
+        for el in nam:
+            if el == '&':
+                sub=''
+                inSub=True
+            elif el ==' ' or el == '\n':
+                if inSub:
+                     subGroups.append(sub)
+                inSub=False
+            elif inSub:
+                 sub+=el
+            else:
+                 exit
+        print(subGroups)
+        # Check duplicates of sub-groups:
+        if len(list(duplicates(subGroups))) != 0:
+            print('!!!!! ERROR: DUPLICATES OF SUB-GROUPS !!!!!')
+            print(list(duplicates(subGroups)))
+        else:
+            print(' Sub-groups OK (no duplicates)')
+              
 
 def checkValues(nam):
         """
